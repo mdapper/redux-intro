3fd44f0c9ae0c104e9f96425da80ce03af08f2fe
Marcelo Dapper <marcelo.dapper@gmail.com>
2017-02-11 16:58:25 -0300

    Add Babel and Webpack configuration files.

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './src/index.js',
+    // the entry point of our app
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js']
+  },
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          "babel-loader",
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            "css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]",
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            }
+          ]
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new ExtractTextPlugin("public/css/style.css"),
+  ],
+};
